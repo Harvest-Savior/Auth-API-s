bf353ae434ae1362ad5a78a70a5e54dc4713e625
carlos.daeli <carlosdaeli@gmail.com>
2024-06-20 19:50:39 +0700

    update database

--- config/db.js
+++ config/db.js
@@ -1,11 +1,15 @@
 import {Sequelize} from "sequelize"
+import dotenv from "dotenv"
+dotenv.config()
 
 // Replace username and password with your actual credentials
-const username = 'root';
-const password = 'hv12345';
+const username = process.env.DB_USER
+const password = process.env.DB_PASSWORD
+const db_name = process.env.DB_NAME
+const host = process.env.DB_HOST
 
-const db = new Sequelize('hs_db', username, password, {
-    host: '34.101.249.52', // Replace with your actual instance connection name
+const db = new Sequelize(db_name, username, password, {
+    host: host, // Replace with your actual instance connection name
     dialect: 'mysql',
 });
 
